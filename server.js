fc0e64c5da9a82f3c6c58ea76905e3216e3c2153
JsDaumi <dlwhdtjd812@gmail.com>
2020-03-31 18:34:28 +0900

    express_static

---
+++ server.js
@@ -0,0 +1,24 @@
+//express를 모듈을 가지고 온다.
+var express = require("express");
+
+var app = express();
+var http = require("http");
+var path = require("path"); //경로작업
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+
+// 아래 순서
+app.use(logger("dev"));
+// css, js, image등 정적 파일 접근
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/main.html");
+});
+
+app.listen(3000, function() {
+  console.log("서버 Start __dirname: ", __dirname);
+});
